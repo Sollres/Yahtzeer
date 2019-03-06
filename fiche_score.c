d1bafdfc8740f05cc01df8bb4dd7aa957978bb3c
Sollres <48085114+Sollres@users.noreply.github.com>
2019-03-06 15:59:14 +0100

    Update fiche_score.c

--- fiche_score.c
+++ fiche_score.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <"fiche_score.h">
 
-typedef struct fiche_score fiche_score;
-struct fiche_score
+typedef struct s_fiche_score
 {
 	int t_un; //additionne les 1 obtenus ex: 3 dés sur 1 donne 3 pts
 	int t_deux; //additionne les 2 obtenus
@@ -11,6 +11,7 @@ struct fiche_score
 	int t_cinq ; //additionne les 5 obtenus
 	int t_six ; //additionne les 6 obtenus
 	int total_i;
+	int Bonus;
 
 	int brelan ; //Total des 3 dés identiques
 	int carre ; //Total des 4 dés identiques
@@ -22,9 +23,9 @@ struct fiche_score
 	int total_ii ;
 
 	int total; //Somme des deux parties
-};
+}fiche_score;
 
-int initialisation_f(fiche_score * score){
+void initialisation_f(fiche_score * score){
 	score->t_un = 0;
 	score->t_deux = 0;
 	score->t_trois = 0;
@@ -32,6 +33,7 @@ int initialisation_f(fiche_score * score){
 	score->t_cinq = 0;
 	score->t_six = 0;
 	score->total_i = 0;
+	score->Bonus = 0;
 
 	score->brelan = 0;
 	score->carre = 0;
@@ -43,4 +45,30 @@ int initialisation_f(fiche_score * score){
 	score->total_ii = 0;
 
 	score->total = 0;
+}
+
+void affichage_score(fiche_score * score){
+	printf("Total de 1 : %i \n",score->t_un);
+	printf("Total de 2 : %i \n",score->t_deux);
+	printf("Total de 3 : %i \n",score->t_trois);
+	printf("Total de 4 : %i \n",score->t_quatre);
+	printf("Total de 5 : %i \n",score->t_cinq);
+	printf("Total de 6 : %i \n",score->t_six);
+	printf("Total intermédiaire : %i \n",score->total_i);
+	printf("Bonus : %i \n",score->Bonus);
+	printf("Brelan (Total des 3 dés) : %i \n",score->brelan);
+	printf("Carré (Total des 4 dés) : %i \n",score->carre);
+	printf("Full (25 points) : %i\n",score->full);
+	printf("Petite Suite (30 points) : %i\n",score->petite_s);
+	printf("Grande Suite (40 points) : %i\n",score->gde_s);
+	printf("Yams (50 points) : %i\n",score->yams);
+	printf("Chance (Total des 5 dés) : %i\n",score->chance);
+	printf("Total Partie Secondaire : %i\n",score->total_ii);
+	printf("Total : %i\n",score->total);
+}
+
+int main(){
+	fiche_score score;
+	initialisation_f(&score);
+	affichage_score(&score);
 }
