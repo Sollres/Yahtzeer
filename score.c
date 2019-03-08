85d549073dd1bd375c7a9ed47fb2e8c4870bca7d
Sollres <48085114+Sollres@users.noreply.github.com>
2019-03-08 13:48:33 +0100

    Add files via upload

---
+++ score.c
@@ -0,0 +1,20 @@
+
+
+typedef struct s_combinaison
+{
+	int t_un; 
+	int t_deux; 
+	int t_trois; 
+	int t_quatre; 
+	int t_cinq ; 
+	int t_six ; 
+
+	int brelan ; 
+	int carre ; 
+	int full ; 
+	int petite_s ; 
+	int gde_s ;
+	int yams ; 
+	int chance ; ;
+
+}combinaison;
