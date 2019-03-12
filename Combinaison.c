63fda296fea7de02dfb74f12e2e4a0af1a53e4dd
Haxxor72 <46872331+Haxxor72@users.noreply.github.com>
2019-03-12 14:39:25 +0100

    Update Combinaison.c

--- Combinaison.c
+++ Combinaison.c
@@ -162,6 +162,84 @@ void chance(combinaison *combi)
 
 }
 
+//fonction qui calcule le point du joueur en fonction de sa main et des combinaison //qu'il peut réaliser
+void point (fiche_score * score, combinaison combi, t_main m1){
+        int choix;
+        if(combi.t_un != 0){                    //verification de la combinaison et affichage de cette combinaison si c'est possible.
+                printf("1 - Total de 1 ?\n");   
+        }
+        if(combi.t_deux != 0){
+                printf("2 - Total de 2 ?\n");
+        }
+        if(combi.t_trois != 0){
+                printf("3 - Total de 3 ?\n");
+        }
+        if(combi.t_quatre != 0){
+                printf("4 - Total de 4 ?\n");
+        }
+        if(combi.t_cinq != 0){
+                printf("5 - Total de 5 ?\n");
+        }
+        if(combi.t_six != 0){
+                printf("6 - Total de 6 ?\n");
+        }
+        if(combi.brelan != 0){
+                printf("7 - Brelan ?\n");
+        }
+        if(combi.carre != 0){
+                printf("8 - Carre ?\n");
+        }
+        if(combi.full != 0){
+                printf("9 - Full ?\n");
+        }
+        if(combi.petite_s != 0){
+                printf("10 - Petite Suite ?\n");
+        }
+        if(combi.gde_s != 0){
+                printf("11 - Grande Suite ?\n");
+        }
+        if(combi.yams != 0){
+                printf("12 - Yams ?\n");
+        }
+        if(combi.chance != 0){
+                printf("13 - Chance ?\n");
+        }
+        printf("Ou souhaitez vous mettre vos points ? \n");
+        scanf("%i",&choix);
+        switch(choix){                                                  // permet d'ecrire les points;
+                case 1 : score->t_un = combi.t_un * 1;break;
+                case 2 : score->t_deux = combi.t_deux * 2;break;
+                case 3 : score->t_trois = combi.t_trois * 3;break;
+                case 4 : score->t_quatre = combi.t_quatre * 4;break;
+                case 5 : score->t_cinq = combi.t_cinq * 5;break;
+                case 6 : score->t_six = combi.t_six * 6;break;
+                case 7 : score->brelan = combi.brelan;break;
+                case 8 : score->carre = combi.carre;break;
+                case 9 : score->full = 25;break;
+                case 10 : score->petite_s = 30;break;
+                case 11 : score->gde_s = 40;break;
+                case 12 : score->yams = 50;break;
+                case 13 : score->chance = m1.de1 + m1.de2 + m1.de3 + m1.de4 + m1.de5;break;
+        }
+    
+        // total de point du première partie de la fiche.
+        score->total_i = score->t_un + score->t_deux + score->t_trois + score->t_quatre + score->t_cinq + score->t_six;  
+        if(score->total_i > 63){
+                score->Bonus = 35;
+        }
+        if(score->Bonus == 35){
+                score->total_i +=35;
+                score->Bonus = 1;
+        }
+    
+        // total de point du second partie.
+        score->total_ii = score->brelan + score->carre + score->full + score->petite_s + score->gde_s + score->yams + score->chance;
+
+        score->total = score->total_i + score->total_ii;        // total final des points.
+
+}
+
+
 
 // vérifie quelle combinaison peut être fait par le joueur.
 void combinaison_f(combinaison * combi, t_main m1, fiche_score score)
